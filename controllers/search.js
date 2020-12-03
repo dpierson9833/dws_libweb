5af86dd51c81e65956d378748cf529625386caba
David Pierson <david.pierson9833@gmail.com>
2020-12-03 14:27:32 -0500

    Add files via upload

--- controllers/search.js
+++ controllers/search.js
@@ -5,9 +5,22 @@ const db = require('../middleware/dbconnect');
 
 
 exports.searchinstance_submit = function(req, res, next) {
-    
-    var search_term = "%" + req.query.q + "%";
-    var sql = "SELECT * FROM books WHERE title LIKE ?";
+
+    //TODO: rewrite this to be more concise
+    if (req.query.filter == 'ISBN'){
+        var search_term = "%" + req.query.q + "%";
+        var sql = "SELECT * FROM books WHERE ISBN LIKE ?";
+    }else if (req.query.filter == 'Author'){
+        var search_term = "%" + req.query.q + "%";
+        var sql = "SELECT * FROM books WHERE author LIKE ?";
+    }else if (req.query.filter == 'Genre'){
+        var search_term = "%" + req.query.q + "%";
+        var sql = "SELECT * FROM books WHERE genre LIKE ?";
+    }else{ //DEFAULT CASE
+        var search_term = "%" + req.query.q + "%";
+        var sql = "SELECT * FROM books WHERE title LIKE ?";
+    }
+
 
     //"SELECT * FROM 'books' WHERE 'title' LIKE CONCAT(?, '%')"
     db.query(sql, search_term, function (err, data, fields) {
